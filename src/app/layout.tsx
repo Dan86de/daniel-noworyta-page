708ef38ab23e6e581b6abe86d09a45b1d9963a62
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-04-18 12:35:39 +0200

    Add speed insights.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "@/styles/tailwind.css";
 
@@ -34,6 +35,7 @@ export default function RootLayout({
           </div>
         </Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
