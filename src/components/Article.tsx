28b51130d5ae471d26d0ce0ccac33b9048d7c3a9
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-04-18 17:21:18 +0200

    Fix articles.

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -5,7 +5,7 @@ import { formatDate } from "@/lib/formatDate.ts";
 export function Article({ article }: { article: ArticleWithSlug }) {
   return (
     <Card as="article">
-      <Card.Title href={`/articles/${article.slug}`}>
+      <Card.Title href={`/artykuly/${article.slug}`}>
         {article.title}
       </Card.Title>
       <Card.Eyebrow as="time" dateTime={article.date} decorate>
