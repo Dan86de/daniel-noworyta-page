b025a923d5178a2967f7ef3d4e07b400e13101a4
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-04-19 11:58:29 +0200

    Improve cookie banner.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,6 +8,8 @@ const variantStyles = {
     "bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70",
   secondary:
     "bg-zinc-100 font-medium text-zinc-900 hover:bg-zinc-200 active:bg-zinc-200 active:text-zinc-900/60 dark:bg-zinc-800/75 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/75 dark:active:text-zinc-50/70",
+  accept:
+    "bg-green-500 text-white hover:bg-green-600 active:bg-green-500 dark:bg-green-500 dark:hover:bg-green-600 dark:active:bg-green-500",
 };
 
 type ButtonProps = {
--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -27,10 +27,10 @@ export function Banner() {
   return (
     <>
       {consentGiven === "undecided" && (
-        <div className="fixed inset-x-0 bottom-0 flex justify-center gap-x-8 gap-y-4 border border-zinc-300 p-6 dark:border-zinc-700/70 text-zinc-900 dark:text-zinc-100 bg-zinc-50 dark:bg-zinc-950 md:flex-row md:items-center ">
+        <div className="fixed inset-x-0 bottom-0 flex justify-center gap-x-8 gap-y-4 border border-zinc-300 p-6 dark:border-zinc-700/70 text-zinc-900 dark:text-zinc-100 bg-zinc-50 dark:bg-zinc-950 md:flex-row md:items-center">
           <Card className="pointer-events-auto ">
             <div className="flex gap-4 items-center">
-              <Card.Title>
+              <Card.Description>
                 Korzystając z danielnoworyta.pl zgadzasz się z{" "}
                 <Link
                   className="text-orange-500 underline"
@@ -38,8 +38,10 @@ export function Banner() {
                 >
                   Polityką Prywatności.
                 </Link>
-              </Card.Title>
-              <Button onClick={handleAcceptCookies}>{"OK"}</Button>
+              </Card.Description>
+              <Button variant={"accept"} onClick={handleAcceptCookies}>
+                {"OK"}
+              </Button>
             </div>
           </Card>
         </div>
