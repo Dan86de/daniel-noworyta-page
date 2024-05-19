ddc009e8f6d64f156c00a0e847555f9d4261beca
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-05-19 11:22:40 +0200

    Update suspense.

--- src/components/ConsultationReviewPageHeader.tsx
+++ src/components/ConsultationReviewPageHeader.tsx
@@ -1,17 +1,15 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React, { Suspense } from "react";
+import React from "react";
 
 export function ConsultationReviewPageHeader() {
   const searchParams = useSearchParams();
   return (
-    <Suspense>
-      <h2 className="flex text-md items-center font-semibold text-zinc-900 dark:text-zinc-100 mt-0">
-        Hej {searchParams.get("name") ?? ""}! 👋
-        <br />
-        <br />
-        Dzięki za pozytywną energię na naszym spotkaniu.
-      </h2>
-    </Suspense>
+    <h2 className="flex text-md items-center font-semibold text-zinc-900 dark:text-zinc-100 mt-0">
+      Hej {searchParams.get("name") ?? ""}! 👋
+      <br />
+      <br />
+      Dzięki za pozytywną energię na naszym spotkaniu.
+    </h2>
   );
 }
