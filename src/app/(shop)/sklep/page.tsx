60e40a6fd44c7befa40efe5031c0da10c0649bcc
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-08-09 22:38:48 +0200

    Working on consultation product on shop

--- src/app/(shop)/sklep/page.tsx
+++ src/app/(shop)/sklep/page.tsx
@@ -1,10 +1,31 @@
+import { type StaticImageData } from "next/image";
+
 import { ContainerInner } from "@/components/Container.tsx";
 import { Card } from "@/components/ProductCard.tsx";
+import consultationImage from "@/images/consultation_product_icon.svg";
 
 export default function ShopPage() {
   return (
     <ContainerInner className={"mt-6"}>
-      <Card />
+      <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100 py-8 text-center">
+        Sklep programistyfrontend
+      </h1>
+      <Card
+        url={"/sklep/konsultacje"}
+        title={"Godzina konsultacji"}
+        subtitle={"Tematy w których mogę Ci pomóc:"}
+        price={"197 zł"}
+        sellingPoints={[
+          "Tworzenie skutecznego CV",
+          "Budowa mocnego portfolio",
+          "Rozwijanie umiejętności miękkich",
+          "Budowa silnego wizerunku online",
+          "Rozwijanie umiejętności technicznych",
+        ]}
+        withSale={true}
+        srcImage={consultationImage as StaticImageData}
+        altImage={"Ikona konsultacji"}
+      />
     </ContainerInner>
   );
 }
